c380d3018a54b55576bb64b3bf62bc2d5a526704
Rasibul <rasibul179@gmail.com>
2026-08-24 22:36:15 +0600

    feat: add email verification endpoint to authenticate user email

--- app/modules/auth/router.py
+++ app/modules/auth/router.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
-
+from fastapi import Query
 from app.modules.auth.schemas import (
     RegisterRequest,
     RegisterResponse
 )
 
-from app.modules.auth.service import register_user
+from app.modules.auth.service import register_user, verify_email
 
 
 
@@ -32,4 +32,23 @@ async def register(
     return {
         "message": "Registration successful",
         "user_id": user_id
+    }
+
+
+@router.get(
+    "/verify-email"
+)
+async def verify_email_account(
+    token:str = Query(...)
+):
+
+
+    await verify_email(token)
+
+
+    return {
+
+        "message":
+        "Email verified successfully"
+
     }
